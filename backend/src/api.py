a0dc5fbda9e216571e1585f2a14aae9e0aabe4c0
unknown <1040185058@qq.com>
2020-02-11 21:32:24 +0800

    role and permissions

--- backend/src/api.py
+++ backend/src/api.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+import sys
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -18,7 +19,6 @@ def after_request(response):
   return response
 
 '''
-@TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
@@ -26,7 +26,6 @@ def after_request(response):
 
 ## ROUTES
 '''
-@TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
@@ -49,7 +48,6 @@ def get_drinks():
 
 
 '''
-@TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -72,7 +70,6 @@ def get_drinks_detail():
 
 
 '''
-@TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -82,10 +79,10 @@ def get_drinks_detail():
 '''
 @app.route('/drinks', methods=['POST'])
 def create_drink():
-  body = request.get_json()
+  body = request.json
 
-  new_title = body.get('title', None)
-  new_recipe = body.get('recipe', None)
+  new_title = body['title']
+  new_recipe = json.dumps(body['recipe'])
 
   try:
     drink = Drink(
@@ -98,15 +95,15 @@ def create_drink():
 
     return jsonify({
       'success': True,
-      'drinks': [drink.long]
+      'drinks': [drink.long()]
     })
 
   except Exception:
+    print(sys.exc_info())
     abort(422)
 
 
 '''
-@TODO implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -120,23 +117,25 @@ def create_drink():
 def edit_drink(id):
   try:
     drink = Drink.query.get(id)
+    if drink is None:
+      abort(404)
     body = request.get_json()
     new_title = body.get('title', None)
     new_recipe = body.get('recipe', None)
     if new_title != None:
       drink.title = new_title
     if new_recipe != None:
-      drink.recipe = new_recipe
+      drink.recipe = json.dumps(list(new_recipe))
     drink.update()
   except Exception:
+    print(sys.exc_info())
     abort(422)
   return jsonify({
     'success': True,
-    'drinks': [drink.long]
+    'drinks': [drink.long()]
   })
 
 '''
-@TODO implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -148,7 +147,7 @@ def edit_drink(id):
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 def delete_drink(id):
   try:
-    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    drink = Drink.query.get(id)
 
     if drink is None:
       abort(404)
